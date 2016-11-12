e4445f6f1fe5f3c9c47aa9ecadc81c329251933f
Ivey Topaz <iveytopaz@gmail.com>
2016-11-11 20:18:15 -0500

    building test server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = process.env.PORT || 49151;
+
+const ip = process.env.IP || '127.0.0.1';
+app = express();
+
+app.use(express.static('dist'))
+
+app.listen(port, ip, function() {
+  console.log('The magic is on ' + ip + ':' + port);
+});
