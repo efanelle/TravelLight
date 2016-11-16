c89d461504932a0fbc56ff68c9c0c091a664ae23
Ivey Topaz <iveytopaz@gmail.com>
2016-11-15 23:08:38 -0500

    merge

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 49151;
 const ip = process.env.IP || '127.0.0.1';
 app = express();
 
-app.use(express.static('dist'))
+app.use(express.static('TLdist'))
 
 app.listen(port, ip, function() {
   console.log('The magic is on ' + ip + ':' + port);
