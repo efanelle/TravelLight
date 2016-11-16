e9211427d2d977e081a386b2a798145bb366de10
Erica Fanelle <efanelle@gmail.com>
2016-11-16 16:35:32 -0500

    changed file structure to allow for routing

--- src/app/secondPage/second.page.component.ts
+++ src/app/secondPage/second.page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-results',
   templateUrl: './second.page.component.html',
   styleUrls: ['./second.page.component.css']
 })
-export class AppComponent {
+export class ResultsComponent {
   // we can pass information from here to the page as shown below:
   // title = 'app works!'; //{{title}} in html
 }
