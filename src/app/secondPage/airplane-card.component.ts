926852faecdbd813f1d41fc894e56f4ed637678f
Ivey Topaz <iveytopaz@gmail.com>
2016-11-17 15:15:57 -0500

    Show image for data card

--- src/app/secondPage/airplane-card.component.ts
+++ src/app/secondPage/airplane-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { D3VisComponent } from './d3-vis.component';
+import { CostInfoService } from './cost-info.service';
 
 @Component({
   selector: 'app-airplane-card',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
     <div class = 'outer'>
       <div class='tab'>
         <img class='bigplane' src='../assets/plane2.png'/>
+        <p>Ranking: {{ ranking }}</p>
         <p>PLANE</p>
         <img class='smplane' src='../assets/airplaneIcon.png'/>
       </div>
@@ -29,9 +32,10 @@ import { Component, OnInit } from '@angular/core';
       box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
     }
     p{
+      display: inline;
       margin-right: 5%;
       text-align:right;
-      font-size:.5em;
+      font-size:1.2em;
       margin-bottom:0;
     }
     .smplane {
@@ -54,8 +58,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirplaneCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private costInfoService: CostInfoService) { }
+  ranking: number = 0;
   ngOnInit() {
+    this.costInfoService.getCosts()
+    .then(costs => {
+      let data: any[] = costs.normalizedData
+      let index: number = 0;
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].label === 'plane') {
+          index = i;
+          break;
+        }
+      }
+      let scores = data.map(methodData => 
+        methodData.data.reduce((a, b) => a + b))
+      let rankings = scores.map(score => {
+        let rank = 1;
+        scores.forEach(compScore => {
+          if (compScore < score) rank++;
+        })
+        return rank;
+      })
+      this.ranking = rankings[index]
+    })
   }
 
 }
