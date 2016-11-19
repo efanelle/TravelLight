0bed83f8b81120f17a4a51e544276229a0d7ea3f
katgurdak <katgurdak@gmail.com>
2016-11-19 15:45:01 -0800

    Show progress on main comparison, stats, and ranking visuals

--- src/app/secondPage/comparison-card.component.ts
+++ src/app/secondPage/comparison-card.component.ts
@@ -6,19 +6,28 @@ import { RadarChartComponent } from './radar-chart.component';
   templateUrl: './comparison-card.component.html',
   styleUrls: ['./comparison-card.component.css']
 })
-export class ComparisonCardComponent implements OnChanges {
+export class ComparisonCardComponent {
   constructor() { }
-
   @Input() costData: any;
   ranking: number = 0;
 
-  ngOnChanges() {
+  public travelMode:string = 'plane';
+
+  public changeTravelMode():void {
+    if (this.travelMode === 'plane') {
+      this.travelMode = 'car';
+    } else {
+      this.travelMode = 'plane';
+    }
+  }
+
+  ngDoCheck() {
     // TODO: Ivey factor this out into it's own function
     if (this.costData) {
       let averageData: any[] = this.costData.normalizedData
       let index: number = 0;
       for (var i = 0; i < averageData.length; i++) {
-        if (averageData[i].label === 'car') {
+        if (averageData[i].label === 'plane') {
           index = i;
           break;
         }
@@ -36,3 +45,23 @@ export class ComparisonCardComponent implements OnChanges {
     }
   }
 }
+
+
+//       for (var i = 0; i < averageData.length; i++) {
+//         if (averageData[i].label === 'plane') {
+//           index = i;
+//           break;
+//         }
+//       }
+//       let scores = averageData.map(methodData => 
+//         methodData.data.reduce((a, b) => a + b))
+//       let rankings = scores.map(score => {
+//         let rank = 1;
+//         scores.forEach(compScore => {
+//           if (compScore < score) rank++;
+//         })
+//         return rank;
+//       })
+//       this.ranking = rankings[index]
+//     }
+//   }
