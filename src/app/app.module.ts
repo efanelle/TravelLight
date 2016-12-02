c07ad7556d8f36814c967b6368244820250ad6e8
Erica Fanelle <efanelle@gmail.com>
2016-12-01 22:54:36 -0500

    Fix merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,6 +49,8 @@ import { CarInputComponent } from './secondPage/car-input/car-input.component';
 import { Card } from '../../node_modules/ng2-card/ng2-card'
 
 
+
+
 // Services
 import { CostInfoService } from './secondPage/cost-info.service';
 import { AirportLocationService } from './landingPage/airport-location.service';
@@ -97,6 +99,7 @@ import { FooterComponent } from './footer/footer.component';
     AdvancedOptionsComponent,
     CarInputComponent,
     Card
+
 
   ],
   imports: [
